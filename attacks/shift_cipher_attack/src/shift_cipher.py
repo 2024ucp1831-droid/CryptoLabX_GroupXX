7da966275492e5ec01740ce671960edb3ddf3196
Yashbaror <2024ucp1831@mnit.ac.in>
2026-08-19 11:37:22 +0530

    Implemented Shift Cipher

---
+++ attacks/shift_cipher_attack/src/shift_cipher.py
@@ -0,0 +1,26 @@
+def encrypt(text, key):
+    result = ""
+
+    for char in text:
+        if char.isupper():
+            result += chr((ord(char) - ord('A') + key) % 26 + ord('A'))
+        elif char.islower():
+            result += chr((ord(char) - ord('a') + key) % 26 + ord('a'))
+        else:
+            result += char
+
+    return result
+
+
+def decrypt(text, key):
+    return encrypt(text, -key)
+
+
+if __name__ == "__main__":
+    plaintext = input("Enter plaintext: ")
+    key = int(input("Enter key: "))
+
+    ciphertext = encrypt(plaintext, key)
+
+    print("Ciphertext:", ciphertext)
+    print("Decrypted:", decrypt(ciphertext, key))
